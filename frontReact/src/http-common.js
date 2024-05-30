3d58aa342a4de71d44222bbae60383e89f1c8cb3
Raffel222 <Raffel222@gmail.com>
2024-05-30 22:22:30 +0200

    Poprawa portu

--- frontReact/src/http-common.js
+++ frontReact/src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 //const applicationURL_AWS = "https://springawslibraryserver5000-env.eba-jm3u5meh.eu-central-1.elasticbeanstalk.com/api"
-const applicationURL_local = "http://localhost:5001/api"
+const applicationURL_local = "http://localhost:5000/api"
 
 
 let httpCommon = axios.create({
